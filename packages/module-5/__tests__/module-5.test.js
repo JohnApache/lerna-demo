ee6e986dc37bfcafd114867e4fe0ecec6c31ad81
cjw <1468733552@qq.com>
2020-02-10 20:33:55 +0800

    feat: add module-5
    ad module-5 for lerna-demo

---
+++ packages/module-5/__tests__/module-5.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const module5 = require('..');
+
+describe('module-5', () => {
+    it('needs tests');
+});
---
+++ packages/module-5/lib/module-5.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = module5;
+
+function module5() {
+    // TODO
+}
